252fc62ba3eec7c4761ac462374707ee05f1bc6d
williamdjones <derek.jones4@uky.edu>
2016-10-15 22:18:54 -0400

    whoops…

--- abc_output.R
+++ abc_output.R
@@ -1,5 +1,5 @@
 library(ggplot2)
-x = read.csv("/Users/derekjones2025/OneDrive/CS463/finalproject/unixABC/unixABC/abcalgodata.csv")
+x = read.csv("/path/to/unixABC/abcalgodata.csv")
 
 
 qplot(x$Colony.Size, x$Error..distance.from.zero) + geom_line(linetype = "dotted")+ geom_point(size = 3, colour ="darkred") + labs(x = "Colony Size", y =" Distance from optimal solution") + ggtitle("Colony Size vs Solution Quality")
